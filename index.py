2316dad6ee34163e0ba2634985e23a6af05663a1
YUGESHKARAN <yugeshkaran01@gmail.com>
2025-01-15 18:20:04 +0530

    updated - 4 groq-api-key set correctly

--- index.py
+++ index.py
@@ -5,42 +5,15 @@ from langchain_core.messages import AIMessage, HumanMessage
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.output_parsers import StrOutputParser
-# from mongodb_database import MongoDBDatabase
+from mongodb_database import MongoDBDatabase
 from langchain_groq import ChatGroq
 import os
-from pymongo import MongoClient
-
-class MongoDBDatabase:
-    def __init__(self, uri, db_name):
-        self.client = MongoClient(uri)
-        self.db = self.client[db_name]
-
-    def get_collection_schema(self, collection_name):
-        # Example method to get a sample document to infer schema
-        sample_document = self.db[collection_name].find_one()
-        return sample_document if sample_document else "No schema available"
-
-    def run(self, collection_name, query ):
-        # Example method to execute a MongoDB query
-        collection = self.db[collection_name]
-        # results = list(collection.find({'authorname': 'Yugesh Karan', 'posts.title': 'Dimensionality Reduction'}, {'posts.$': 1, '_id': 0}))
-        # results =list(collection.find({'authorname': "haricharan_1133"}))
-        # results =list(collection.find({'authorname': 'Yugesh Karan'}))
-        # results =list(collection.find({'authorname': 'Yugesh Karan'}))
-        # results =list(collection.distinct("authorname"))
-        # results =list(collection.find({ 'posts.title': 'Dimensionality Reduction' }))
-        
-        results =list(eval(query))
-        return results
-
-# Usage Example
-# db = MongoDBDatabase("mongodb://localhost:27017", "your_database_name")
 
 
 load_dotenv()
 
 # Set API keys
-os.environ['GROQ_API_KEY'] = os.getenv('GROQ_API_KEY')
+os.environ['groq_api_key'] = os.getenv('groq_api_key')
 os.environ['MONGODB_URI'] = os.getenv('MONGODB_URI')
 
 app = Flask(__name__)
